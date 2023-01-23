fe6ca71544deaff277e2e5f7615120673337c02f
Rano Khassanova <khasanova.rano@gmail.com>
2023-01-23 15:35:43 +0600

    o

---
+++ todolist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(max_length=1000, null=False, blank=False)
+    creation_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    last_update = models.DateTimeField(auto_now=True, null=False, blank=False)
+
+    class Meta:
+        db_table = 'Todos'
+
+
---
+++ todolist/views.py
@@ -0,0 +1,63 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.mixins import UpdateModelMixin, DestroyModelMixin
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListView(
+    APIView, UpdateModelMixin, DestroyModelMixin
+):
+    def get(self, request, id=None):
+        if id:
+            try:
+                queryset = Todo.objects.get(id=id)
+            except Todo.DoesNotExist:
+                return Response({'errors': 'This todo item does not exist.'}, status=400)
+
+            read_serializer = TodoSerializer(queryset)
+
+        else:
+            queryset = Todo.objects.all()
+            read_serializer = TodoSerializer(queryset, many=True)
+
+        return Response(read_serializer.data)
+
+    def post(self, request):
+        create_serializer = TodoSerializer(data=request.data)
+
+        if create_serializer.is_valid():
+            todo_item_object = create_serializer.save()
+            read_serializer = TodoSerializer(todo_item_object)
+
+            return Response(read_serializer.data, status=201)
+
+        return Response(create_serializer.errors, status=400)
+
+    def put(self, request, id=None):
+        try:
+            todo_item = Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return Response({'errors': 'This todo item does not exist.'}, status=400)
+
+        update_serializer = TodoSerializer(todo_item, data=request.data)
+
+        if update_serializer.is_valid():
+            todo_item_object = update_serializer.save()
+
+            read_serializer = TodoSerializer(todo_item_object)
+
+            return Response(read_serializer.data, status=200)
+
+        return Response(update_serializer.errors, status=400)
+
+    def delete(self, request, id=None):
+        try:
+            todo_item = Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return Response({'errors': 'This todo item does not exist.'}, status=400)
+
+        todo_item.delete()
+
+        return Response(status=204)
